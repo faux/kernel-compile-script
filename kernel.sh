0715b0e08b3e5bfc93fe1f4ae5c5880a6189cf55
faux <faux@fauxsoft.com>
2009-01-28 08:25:50 -0500

    moved variables around

--- kernel.sh
+++ kernel.sh
@@ -2,17 +2,18 @@
 COMMON_PACKAGES="build-essential bin86 kernel-package wget curl"
 CONSOLE_PACKAGES="libncurses5 libncurses5-dev"
 X_PACKAGES="libqt3-headers libqt3-mt-dev"
+
 apt-get install $COMMON_PACKAGES $CONSOLE_PACKAGES
 
 cd /usr/src
 
 DOWNLOAD_PATH=`curl kernel.org | grep "/pub/linux/kernel/" | grep ">F<" | cut -d'"' -f2 | head -n 1`
 KERNEL_FILENAME=`echo $DOWNLOAD_PATH | tr "/" "\n" | tail -n 1`
-
-#wget -c http://kernel.org$DOWNLOAD_PATH
-#tar -xvjf $KERNEL_FILENAME
 KERNEL_FIELDCNT=`echo $KERNEL_FILENAME | tr "." "\n" | head -n -2 | wc -l`
 KERNEL_DIR=`echo $KERNEL_FILENAME | tr "." "\n" | head -n -2 |  tr "\n" "." | cut -d"." --field=-$KERNEL_FIELDCNT`
+
+wget -c http://kernel.org$DOWNLOAD_PATH
+tar -xvjf $KERNEL_FILENAME
 echo
 echo $KERNEL_DIR
 cd $KERNEL_DIR
